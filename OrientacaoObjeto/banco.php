a813ad36373ecbfa1d6390ef447152ea0b4769d0
lhenriquesouza <lhsouza005@gmail.com>
2022-02-15 16:43:33 -0300

    Continuando os trabalhos

--- OrientacaoObjeto/banco.php
+++ OrientacaoObjeto/banco.php
@@ -1,14 +1,15 @@
 <?php
 
-require_once 'src/Conta.php';
-require_once 'src/Endereco.php';
-require_once 'src/Titular.php';
-require_once 'src/Cpf.php';
-require_once 'src/Pessoa.php';
+require_once 'autoload.php';
+
+use Alura\Banco\Modelo\Conta\Titular;
+use Alura\Banco\Modelo\Endereco;
+use Alura\Banco\Modelo\Cpf;
+use Alura\Banco\Modelo\Conta\Conta;
 
 $endereco = new Endereco('PF', 'Nova Gimirim', 'Rua Prefeito', '632');
-$primeiraConta = new Conta(new Titular('Luis Henrique', new Cpf('123.456.789-10'), $endereco));
-
+$Luis = new Titular('Luis Henrique', new Cpf('123.456.789-10'), $endereco);
+$primeiraConta = new Conta($Luis);
 $primeiraConta->deposita(500);
 $primeiraConta->saca(300); // isso é ok
 
@@ -16,7 +17,7 @@ echo $primeiraConta->recuperaNomeTitular() . PHP_EOL;
 echo $primeiraConta->recuperaCpfTitular() . PHP_EOL;
 echo $primeiraConta->recuperaSaldo() . PHP_EOL;
 
-$segundaConta = new Conta(new Titular('Gabrielle', new Cpf('123.456.654-20'), $endereco));
+$segundaConta = new Titular('Gabrielle', new Cpf('123.456.654-20'), $endereco);
 var_dump($segundaConta);
 
 echo Conta::recuperaNumeroContas() . PHP_EOL; //pode ser usado assim ou
